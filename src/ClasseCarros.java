b596b786221fa4ef2c8b851361d0d89534bfcee6
KauanSala <salakauan3@gmail.com>
2025-09-11 11:57:35 -0300

    Foi adicionado os código da ClasseCaminhoes e ClasseCarro

--- src/ClasseCarros.java
+++ src/ClasseCarros.java
@@ -1,11 +1,15 @@
 
-public class ClasseCarros extends ClasseAutomotores{
+public class ClasseCarros extends ClasseAutomotores {
+ 
 	
-	String 
-	
-	public ClasseCarros() {
+	public ClasseCarros (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		
+		super(corPar, marcaPar, modeloPar, tipoPar);
+		
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
 		
 	}
-
-
 }
